4f728e4885398c09c73b9a5bb797775ea186a8be
austinblackdaxko <austin.black@daxko.com>
2022-07-09 20:11:59 -0500

    started section 16 on conditionals

---
+++ Section-16/conditionals.js
@@ -0,0 +1,15 @@
+console.log('It works')
+
+let random = Math.random();
+
+if (random < 0.5) {
+    console.log(`Your number is ${random}, less than 5`)
+}
+if (random >= 0.5) {
+    console.log(`Your number is ${random}, GREATER than 5`)
+}
+
+
+
+
+console.log('AFTER CONDITIONAL')
