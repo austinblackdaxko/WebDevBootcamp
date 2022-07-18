d59d9c926529c6ff136d92ca7a8be32ccdbbafc5
austinblackdaxko <austin.black@daxko.com>
2022-07-17 19:43:15 -0500

    added to conditionals.js

--- Section-16/conditionals.js
+++ Section-16/conditionals.js
@@ -1,15 +1,43 @@
-console.log('It works')
+// console.log('It works')
 
-let random = Math.random();
+// let random = Math.random();
 
-if (random < 0.5) {
-    console.log(`Your number is ${random}, less than 5`)
+// if (random < 0.5) {
+//     console.log(`Your number is ${random}, less than 5`)
+// }
+// if (random >= 0.5) {
+//     console.log(`Your number is ${random}, GREATER than 5`)
+// }
+
+// console.log('AFTER CONDITIONAL')
+
+const dayOfWeek = prompt('Enter a Day!').toLowerCase();
+
+if (dayOfWeek === 'monday') {
+    console.log('Ugghhh, I hate Mondays')
+} 
+else if (dayOfWeek === 'saturday') {
+    console.log('YAY I LOVE SATURDAYS')
 }
-if (random >= 0.5) {
-    console.log(`Your number is ${random}, GREATER than 5`)
+else if (dayOfWeek === 'friday') {
+    console.log('FRIDAYS ARE DECENT, ESCPECIIALY AFTER WORK!!')
+}
+else {
+    console.log('MEH')
 }
 
 
+// const age = 4;
 
-
-console.log('AFTER CONDITIONAL')
+// if (age < 5) {
+//     console.log('you are a baby. you get in for free')
+// }
+// else if (age < 10) {
+//     console.log('You are a child, you pay 10 dollars')
+// }
+// else if (age < 65) {
+//     console.log('you are an adult, you pay 20 dollars')
+// }
+// else if (age >= 65) {
+//     console.log('you are a senior, you pay 10 dollars')
+// }
