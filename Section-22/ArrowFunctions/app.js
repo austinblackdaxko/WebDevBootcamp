c5c4f05e2f7dcd73d4063924e790e4c9f6f84c78
austinblackdaxko <austin.black@daxko.com>
2022-09-09 16:50:58 -0500

    Added Callbacks,  Reduce, SomeEvery sections. modified ArrowFunctions/app.js to include using 'this' keyword in an arrow function

--- Section-22/ArrowFunctions/app.js
+++ Section-22/ArrowFunctions/app.js
@@ -17,3 +17,17 @@ const rollDie = () => (
     Math.floor(Math.random() * 6) + 1
 )
 
+// section for this and arrow functions
+
+const person = {
+    firstName: 'Viggo',
+    lastName: 'Mortensen',
+    fullName: function() {
+        return `${this.firstName} ${this.lastName}`
+    },
+    shoutName: function () {
+        setTimeout( () => {
+            console.log(`${this.fullName().toUpperCase()}!!!!`)
+        }, 3000)
+    }
+}
