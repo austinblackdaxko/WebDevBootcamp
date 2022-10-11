d700609c9ba5b3cc0b7088e477b7e8127768eec1
austinblackdaxko <austin.black@daxko.com>
2022-10-10 21:23:57 -0500

    Section 24

---
+++ Section-24/MyClass.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyClass {
+
+private Map map;
+
+public MyClass() {
+map = new HashMap<>();
+map.put("foo", 1);
+map.put("bar", 3);
+}
+
+public int getValue(String input, int numRetries) throws Exception {
+    try {
+        return map.get(input).intValue();
+    }
+    catch (Exception e) {
+        if (numRetries > 3) {
+            throw e;
+        }
+    return getValue(input, numRetries + 1);
+    }
+}
+
+}
