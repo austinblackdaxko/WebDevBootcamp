0cbf8dc98a4dbcb3e7453ba12e0ddef7595a2486
Austin Black <austin.black@daxko.com>
2022-10-29 13:14:11 -0500

    section 24 complete

---
+++ Section-24/Pokemon Sprites/app.js
@@ -0,0 +1,17 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+let div = document.querySelector('.pokemonJail')
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon'
+
+
+for (let i = 0; i < 151; i++) {
+  const pokemon = document.createElement('div')
+  pokemon.classList.add('pokemon')
+  const label = document.createElement('span')
+  const newImg = document.createElement('img');
+  newImg.src = `${baseURL}/${i + 1}.png`
+  label.innerText = `${i + 1}`
+  pokemon.appendChild(newImg)
+  pokemon.appendChild(label)
+  div.append(pokemon)
+}
