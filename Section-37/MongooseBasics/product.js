e47006b25cb2e3b7c22d557dfd9172e08f4ba41a
Austin Black <austin.black@daxko.com>
2023-01-28 12:09:41 -0600

    completed Section-37

--- Section-37/MongooseBasics/product.js
+++ Section-37/MongooseBasics/product.js
@@ -10,26 +10,83 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopApp').then(() => {
 const productSchema = new mongoose.Schema({
   name: {
     type:String,
-    required: true 
+    required: true,
+    maxlength:  20
   },
   price: {
     type: Number,
+    min: [0, 'Price must be positive you dingus']
   },
   isOnSale: {
-    type: Boolean
+    type: Boolean,
+    default: false
+  },
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0
+    },
+    inStore: {
+      type: Number,
+      default: 0
+    }
+  },
+  size: {
+    type: String,
+    enum: ['S', 'M', 'L']
   }
 })
 
+productSchema.methods.greet = function() {
+  console.log("HELLLO HI HOWDY!")
+  console.log(`- from ${this.name}`)
+}
+
+productSchema.methods.toggleOnSale = function () {
+  this.isOnSale = !this.isOnSale;
+  this.save();
+}
+
+productSchema.methods.addCategory = function(newCat) {
+  this.categories.push(newCat);
+  return this.save();
+}
+
+productSchema.statics.fireSale = function() {
+  return this.updateMany({}, {isOneSale: true, price: 0})
+}
+
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({ price: 599 })
+const findProduct = async () => {
+  const foundProduct = await Product.findOne({name: "Mountain Bike"});
+  await foundProduct.addCategory('Outdoors')
+  console.log(foundProduct)
+}
 
-bike.save()
-.then(data => {
-  console.log("it worked")
-  console.log(data)
-})
-.catch(err => {
-  console.log("oh no, error!")
-  console.log(err)
-})
+Product.fireSale().then(res => console.log(res))
+
+const bike = new Product({ name: 'Biking Shirt', price: 19.50, isOnSale: true, categories: ['Cycling'], qty : {online: 5}, size : 'J' } )
+
+// bike.save()
+// .then(data => {
+//   console.log("it worked")
+//   console.log(data)
+// })
+// .catch(err => {
+//   console.log("oh no, error!")
+//   console.log(err)
+// })
+
+// bike.greet()
+
+// Product.findOneAndUpdate({name: 'Tire Pump'}, {price: -10.99}, { new : true, runValidators: true})
+// .then(data => {
+//   console.log("it worked")
+//   console.log(data)
+// })
+// .catch(err => {
+//   console.log("oh no, error!")
+//   console.log(err)
+// })
