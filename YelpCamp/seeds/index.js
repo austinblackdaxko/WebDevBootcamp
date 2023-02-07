31b58f072e5ae4fb83254f76f4b966012b090611
Austin Black <austin.black@daxko.com>
2023-02-07 06:55:29 -0600

    added styling to current YelpCamp model

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi nulla quaerat repellendus, illum itaque eius et distinctio beatae molestiae nemo dolorum incidunt, numquam quia autem neque mollitia officiis harum rem.',
+      price: price
     });
     await camp.save();
   }
